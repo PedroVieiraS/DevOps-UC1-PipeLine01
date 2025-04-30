95705f7268f10074edeef5ef7ec2ba3df6e0c50f
PEDRO CORREA VIEIRA DOS SANTOS <vieira.pedro@aluno.senac.rio>
2025-04-29 21:29:00 -0300

    teste7

--- gerar_grafico.py
+++ gerar_grafico.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')  
 df['idade'].plot(kind='hist')  
 plt.title('Distribuição de Idades')  
-plt.savefig('grafico.png')  
+plt.savefig('grafico.png')
